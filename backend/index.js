be562930b784ec8d09c8a13557f2bcbfb2912a32
Gonçalo Vicente <50244348+GoncaloVicente@users.noreply.github.com>
2021-05-26 10:28:15 +0100

    Dashboard

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(url, {
 });
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
   next();

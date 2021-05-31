7e62150317f9f627cdca8b0d88d1036574615d1f
Fábio <fabiophenriques@gmail.com>
2021-05-31 14:21:00 +0100

    Organizations of backend

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-var MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/renewable_db";
-
-var response
-
-const client = new MongoClient(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -27,6 +18,10 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
   next();
 });
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+
+const importedFunctions = require('./functions.js')
 
 app.get('/', (req, res) => {
   res.send('Hello World, this is our project of ESS. Polytechnic of Leiria.');
@@ -47,7 +42,7 @@ app.post('/payload', (req, res) => {
     }
   }]
   try {
-    response = addEntry(body)
+    response = importedFunctions.addEntry(body)
     res.send(response)
   } catch (err) {
     console.log(err)
@@ -133,77 +128,3 @@ app.get('/payload/all', (req, res) => {
     console.log(err)
   }
 });
-
-
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
-
-function createCollection() {
-  client.db("renewable_db").createCollection("renewable_db_collection", (err, res) => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(res)
-    }
-  })
-}
-
-async function addEntry(payload) {
-  await client.connect();
-  await client.db("renewable_db").command({ ping: 1 });
-
-  console.log("Connected successfully to Database");
-
-  await client.db("renewable_db").collection("renewable_db_collection").insertOne(payload, function (err, res) {
-    if (err) {
-      console.log(err)
-      return err
-    } else {
-      console.log(res)
-      return res
-    }
-  })
-
-  await client.db.close();
-}
-
-async function readValues() {
-  await client.connect();
-  await client.db("renewable_db").command({ ping: 1 });
-
-  await console.log("Connected successfully to Database");
-
-  await client.db("renewable_db").collection("renewable_db_collection").find({}).toArray(function (err, res) {
-    if (err) {
-      console.log(err)
-      return err
-    } else {
-      console.log("Read values from Database")
-      console.log(res)
-      response = res
-      //return res
-    }
-  });
-
-  await client.db.close;
-}
-
-function readLast() {
-  client.connect();
-  client.db("renewable_db").command({ ping: 1 });
-
-  console.log("Connected successfully to Database");
-
-  client.db("renewable_db").collection("renewable_db_collection").findOne({}, function (err, res) {
-    if (err) {
-      console.log(err);
-      return err;
-    } else {
-      console.log("Read values from Database");
-      console.log(res);
-      response = res
-      //return res;
-    }
-  })
-
-  client.db.close;
-}
